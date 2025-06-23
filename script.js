f93629a2dc0441048bc7a243f7acb7181eeab400
guzovsky <102002643+guzovsky@users.noreply.github.com>
2025-06-23 16:03:14 -0400

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-function showTab(index) {
-    const tabs = document.querySelectorAll('.tab');
-    const contents = document.querySelectorAll('.content');
+const tabs = Array.from(document.querySelectorAll(".tab"));
+const text = document.querySelector(".content-text");
 
-    tabs.forEach((tab, i) => {
-      tab.classList.toggle('active', i === index);
-      contents[i].classList.toggle('active', i === index);
-    });
-  }
+const tabContents = [
+  "This is the content of the First tab: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi, architecto!",
+  "This is the content of the Second tab: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi, architecto!",
+  "This is the content of the Third tab: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi, architecto!",
+  "This is the content of the Fourth tab: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi, architecto!"
+];
+
+tabs.forEach((tab, i) => {
+  tab.addEventListener("click", () => {
+
+    if (tab.classList.contains("active")) {
+      return;
+    }
+
+    tabs.forEach(t => t.classList.remove("active"));
+    tab.classList.add("active");
+
+    // Animate content change
+    text.classList.remove("fade");
+    const content = document.querySelector(".content");
+    content.classList.remove("appear");
+
+    void text.offsetWidth; // force reflow
+
+    text.classList.add("fade");
+    content.classList.add("appear");
+
+    text.textContent = tabContents[i];
+  });
+});
